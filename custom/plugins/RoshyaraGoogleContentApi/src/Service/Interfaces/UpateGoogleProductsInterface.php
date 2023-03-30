a98cc3e3cab9f91f801229a83318dd360954beb4
Dr. Nab Roshyara <mathematikboy@yahoo.com>
2023-03-30 15:24:16 +0000

    Update Google Service implemented in schedule

---
+++ custom/plugins/RoshyaraGoogleContentApi/src/Service/Interfaces/UpateGoogleProductsInterface.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace Roshyara\GoogleContentApi\Service\Interfaces\ServiceInerface;
+interface UpdateGoogleProductsServiceInterface {
+	public function getGoogleMerchantProducts();
+
+}
---
+++ custom/plugins/RoshyaraGoogleContentApi/src/Service/UpdateGoogleProductsService.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace Roshyara\GoogleContentApi\Service;
+//use  Roshyara\GoogleContentApi\Service\ServiceInterfaces\UpdateGoogleProductsInterface;
+
+
+//class UpdateGoogleProductsService implements UpdateGoogleProductsInterface
+class UpdateGoogleProductsService
+{
+    public function __construct(){
+      echo "google Service\n";
+    }	
+    public function getGoogleMerchantProducts(): void
+    {
+   	echo "Google products";      
+   }
+}
